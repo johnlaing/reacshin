1dcdcf3ec0ceee198513f316ae0c0a9b87d47e10
John Laing <john.laing@gmail.com>
2017-05-09 10:25:17 -0400

    add man pg

--- R/reacshin.R
+++ R/reacshin.R
@@ -1,3 +1,13 @@
+##' This function creates an HTML widget binding ReactJS and Shiny
+##'
+##' @title Create reacshin widget
+##' @param type A character indicating the tag to be associated with this
+##' output. Should correspond to a javascript class.
+##' @param data A list of data which will be bound to the output as 'props'.
+##' @param ... Other parameters passed on to \code{htmlwidgets::createWidget}
+##' @return An HTML widget
+##' @author John Laing
+
 reacshin <- function(type, data, ...) {
     htmlwidgets::createWidget("reacshin", list(type=type, data=data), ...)
 }
